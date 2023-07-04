4815158f7d64f71eb1ce5ae8ac922233062ff7af
vanessafcosta <bio.vfc@gmail.com>
2023-07-04 13:19:48 +0100

    Fixed schema and entities, dateCreated and updated post with time

--- src/main/kotlin/com/acebook/entities/Post.kt
+++ src/main/kotlin/com/acebook/entities/Post.kt
@@ -1,10 +1,12 @@
 package com.acebook.entities
 
 import org.ktorm.entity.Entity
+import java.time.LocalDateTime
 
 interface Post : Entity<Post> {
     companion object : Entity.Factory<Post>()
     val id: Int
     var content: String
     var userId: Int
+    var dateCreated: LocalDateTime
 }
