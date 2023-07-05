38e683197d19041a157a48558ac636f591fa4e20
Mumtaz <mumtazob@gmail.com>
2023-07-05 14:40:17 +0100

    created tests for like button feature and adjusted tests on createposttest

--- src/test/kotlin/com/acebook/CreatePostTest.kt
+++ src/test/kotlin/com/acebook/CreatePostTest.kt
@@ -4,13 +4,17 @@ import com.acebook.requiredLoginCredentialsLens
 import com.acebook.requiredSignupFormLens
 import com.acebook.schemas.Posts
 import com.acebook.schemas.Users
+import com.natpryce.hamkrest.assertion.assertThat
+import okhttp3.OkHttpClient
 import org.http4k.client.OkHttp
 import org.http4k.core.*
+import org.http4k.hamkrest.hasStatus
 import org.http4k.lens.WebForm
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.ktorm.database.Database
 import org.ktorm.dsl.deleteAll
+import org.riversun.okhttp3.OkHttp3CookieHelper
 
 class CreatePostTest {
     @BeforeEach
@@ -29,27 +33,55 @@ class CreatePostTest {
     }
 
     @Test
-    fun `Sign up then create a new post`() {
-        val client = OkHttp()
-        client(
+
+    fun `Signup and Login and creating a Post return 200 OK response`() {
+        val cookieHelper = OkHttp3CookieHelper()
+        val client = OkHttp(OkHttpClient().newBuilder().cookieJar(cookieHelper.cookieJar()).build())
+        val form = WebForm(
+            mapOf(
+                "email" to listOf("email"),
+                "password" to listOf("password")
+            ))
+
+        val response: Response = client(
             Request(Method.POST, "http://localhost:9999/users").with(
-                requiredSignupFormLens of WebForm(mapOf(
-                    "email" to listOf("test@acebook.com"),
-                    "password" to listOf("s3cr3tp4ss")
-                ))
+                requiredSignupFormLens of form
             )
+                .header("content-type", "application/x-www-form-urlencoded")
         )
 
-        val loginResponse = client(
+        assertThat(response, hasStatus(Status.OK))
+        assert(response.bodyString().contains("Login"))
+        val form2 = WebForm(
+            mapOf(
+                "email" to listOf("email"),
+                "password" to listOf("password")
+            ))
+
+        val response2: Response = client(
             Request(Method.POST, "http://localhost:9999/sessions").with(
-                requiredLoginCredentialsLens of WebForm(mapOf(
-                    "email" to listOf("test@acebook.com"),
-                    "password" to listOf("s3cr3tp4ss")
-                ))
+                requiredLoginCredentialsLens of form2
+            )
+                .header("content-type", "application/x-www-form-urlencoded")
+        )
+
+        assertThat(response2, hasStatus(Status.OK))
+        assert(response2.bodyString().contains("Acebook"))
+
+        val form3 = WebForm(
+            mapOf(
+                "content" to listOf("content")
             )
         )
 
-        //val response: Response = client(Request(Method.GET, "http://localhost:9999/posts/new"))
+        val response3: Response = client(
+            Request(Method.POST, "http://localhost:9999/posts").with(
+                requiredContentLens of form3
+            )
+                .header("content-type", "application/x-www-form-urlencoded")
+        )
+
+        assert(response3.bodyString().contains("content"))
 
     }
 }
--- src/test/kotlin/com/acebook/PostsFeedTest.kt
+++ src/test/kotlin/com/acebook/PostsFeedTest.kt
@@ -3,17 +3,21 @@ package com.acebook
 import com.acebook.entities.Post
 import com.acebook.schemas.Posts
 import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.contains
 import com.natpryce.hamkrest.containsSubstring
+import okhttp3.OkHttpClient
 import org.http4k.client.OkHttp
 import org.http4k.core.*
 import org.http4k.hamkrest.hasBody
 import org.http4k.hamkrest.hasStatus
+import org.http4k.lens.WebForm
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.ktorm.database.Database
 import org.ktorm.dsl.deleteAll
 import org.ktorm.entity.add
 import org.ktorm.entity.sequenceOf
+import org.riversun.okhttp3.OkHttp3CookieHelper
 
 class PostsFeedTest {
 
@@ -50,4 +54,55 @@ class PostsFeedTest {
         assertThat(response, hasBody(containsSubstring("Test post 1")))
         assertThat(response, hasBody(containsSubstring("Test post 1")))
     }
+
+    @Test
+    fun `User cannot see the like button if not logged in`() {
+        val client = OkHttp()
+
+        val response: Response = client(
+            Request(Method.GET, "http://localhost:9999/")
+        )
+
+        assertThat(response, hasStatus(Status.OK))
+        assertThat(
+            response,
+            hasBody(containsSubstring("Acebook"))
+        )
+    }
+
+    @Test
+    fun `User should only be able to click the button once logged in`() {
+        val cookieHelper = OkHttp3CookieHelper()
+        val client = OkHttp(OkHttpClient().newBuilder().cookieJar(cookieHelper.cookieJar()).build())
+        val form = WebForm(
+            mapOf(
+                "email" to listOf("email"),
+                "password" to listOf("password")
+            ))
+
+        val response: Response = client(
+            Request(Method.POST, "http://localhost:9999/users").with(
+                requiredSignupFormLens of form
+            )
+                .header("content-type", "application/x-www-form-urlencoded")
+        )
+
+        assertThat(response, hasStatus(Status.OK))
+        assert(response.bodyString().contains("Login"))
+        val form2 = WebForm(
+            mapOf(
+                "email" to listOf("email"),
+                "password" to listOf("password")
+            ))
+
+        val response2: Response = client(
+            Request(Method.POST, "http://localhost:9999/sessions").with(
+                requiredLoginCredentialsLens of form2
+            )
+                .header("content-type", "application/x-www-form-urlencoded")
+        )
+
+        assertThat(response2, hasStatus(Status.OK))
+        assert(response2.bodyString().contains("like"))
+   }
 }
