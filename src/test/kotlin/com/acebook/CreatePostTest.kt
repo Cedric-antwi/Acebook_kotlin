151a7aaeea1498134883470548b149199ae2136b
Sayonara <sayow@hotmail.com>
2023-07-10 13:12:24 +0100

    Test fixing all good

--- src/test/kotlin/com/acebook/CreatePostTest.kt
+++ src/test/kotlin/com/acebook/CreatePostTest.kt
@@ -9,15 +9,12 @@ import okhttp3.OkHttpClient
 import org.http4k.client.OkHttp
 import org.http4k.core.*
 import org.http4k.hamkrest.hasStatus
-import org.http4k.lens.MultipartForm
-import org.http4k.lens.Validator
-import org.http4k.lens.WebForm
+import org.http4k.lens.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.ktorm.database.Database
 import org.ktorm.dsl.deleteAll
 import org.riversun.okhttp3.OkHttp3CookieHelper
-import org.http4k.lens.multipartForm
 
 class CreatePostTest {
 
@@ -38,59 +35,54 @@ class CreatePostTest {
 
     @Test
 
-//    fun `Signup and Login and creating a Post return 200 OK response`() {
-//        val cookieHelper = OkHttp3CookieHelper()
-//        val client = OkHttp(OkHttpClient().newBuilder().cookieJar(cookieHelper.cookieJar()).build())
-//        val form = WebForm(
-//            mapOf(
-//                "email" to listOf("test@acebook.com"),
-//                "password" to listOf("s3cr3tp4ss"),
-//                "firstname" to listOf("Tester"),
-//                "lastname" to listOf("User"),
-//                "username" to listOf("Tester_User")
-//            ))
-//
-//        val response: Response = client(
-//            Request(Method.POST, "http://localhost:9999/users").with(
-//                requiredSignupFormLens of form
-//            )
-//                .header("content-type", "application/x-www-form-urlencoded")
-//        )
-//
-//        assertThat(response, hasStatus(Status.OK))
+    fun `Signup and Login and creating a Post return 200 OK response`() {
+        val cookieHelper = OkHttp3CookieHelper()
+        val client = OkHttp(OkHttpClient().newBuilder().cookieJar(cookieHelper.cookieJar()).build())
+        val form = WebForm(
+            mapOf(
+                "email" to listOf("email"),
+                "password" to listOf("password"),
+                "firstname" to listOf("firstname"),
+                "lastname" to listOf("firstname"),
+                "username" to listOf("firstname")
+            ))
+        val response: Response = client(
+            Request(Method.POST, "http://localhost:9999/users").with(
+                requiredSignupFormLens of form
+            )
+                .header("content-type", "application/x-www-form-urlencoded")
+        )
+        assertThat(response, hasStatus(Status.OK))
 //        assert(response.bodyString().contains("Login"))
-//        val form2 = WebForm(
-//            mapOf(
-//                "email" to listOf("email"),
-//                "password" to listOf("password")
-//            ))
-//
-//        val response2: Response = client(
-//            Request(Method.POST, "http://localhost:9999/sessions").with(
-//                requiredLoginCredentialsLens of form2
-//            )
-//                .header("content-type", "application/x-www-form-urlencoded")
-//        )
-//
-//        assertThat(response2, hasStatus(Status.OK))
-//        assert(response2.bodyString().contains("Welcome"))
-//
-//
-//
-//        val form3 = WebForm(
-//            mapOf(
-//                "content" to listOf("content")
-//            )
-//        )
-//
-//        val response3: Response = client(
-//            Request(Method.POST, "http://localhost:9999/posts").with(
-//                MultipartForm()
-//            )
-//                .header("multipart/form-data", "application/x-www-form-urlencoded")
-//        )
-//
-//        assert(response3.bodyString().contains("Welcome"))
-//
-//    }
-//}
+        val form2 = WebForm(
+            mapOf(
+                "email" to listOf("email"),
+                "password" to listOf("password")
+            ))
+        val response2: Response = client(
+            Request(Method.POST, "http://localhost:9999/sessions").with(
+                requiredLoginCredentialsLens of form2
+            )
+                .header("content-type", "application/x-www-form-urlencoded")
+        )
+        assertThat(response2, hasStatus(Status.OK))
+        val nameField = MultipartFormField.string().required("text")
+        val imageFile = MultipartFormFile.optional("picture")
+        val strictFormBody = Body.multipartForm(Validator.Strict, nameField,imageFile).toLens()
+        val multipartform = MultipartForm().with(
+            nameField of ("Hello there").toString(),
+            imageFile of MultipartFormFile(
+                "",
+                ContentType.OCTET_STREAM,
+                "somebinarycontent".byteInputStream()
+            )
+        )
+        val response3: Response = client(
+            Request(Method.POST, "http://localhost:9999/posts").with(
+                strictFormBody of multipartform
+            )
+                .header("content-type", "application/x-www-form-urlencoded")
+        )
+        assert(response3.bodyString().contains("Welcome"))
+    }
+}
